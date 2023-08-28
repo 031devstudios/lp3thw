0ec0fa52dad05539e789175ad6d82bb953b2fa55
Josh Robb <031devstudios@gmail.com>
2023-08-28 20:52:17 +0200

    Added ex3.py

---
+++ Examples/ex3.py
@@ -0,0 +1,32 @@
+# Prints the string
+print("I will now count my chickens:")
+
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4.0)
+
+# Prints the string
+print("Now I will count the eggs:")
+
+#
+print(3 + 2 + 1 - 5 + 4 % 2 - 1  / 4 + 6)
+
+# Prints the string
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Compares the two math problems
+print(3 + 2 < 5 - 7)
+
+# Adds 3 and 2 then subtracts 5 from 7
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+
+# Prints the string
+print("Oh, that's why it's False.")
+
+# Prints the string
+print("How about some more.")
+
+# Checks if the statments are greater or less
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
