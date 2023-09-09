fb803b24e77bd826ad4f3e42af660e1a4033547e
Josh Robb <031devstudios@gmail.com>
2023-09-09 20:34:23 +0200

    Added ex27.py

---
+++ Examples/ex27.py
@@ -0,0 +1,42 @@
+#NOT
+print("NOT")
+print("--------------")
+print(not False)
+print(not True)
+print("--------------")
+
+#OR
+print("OR")
+print("--------------")
+print(True or False)
+print(True or True)
+print(False or True)
+print(False or False)
+print("--------------")
+
+#AND
+print("AND")
+print("--------------")
+print(True and False)
+print(True and True)
+print(False and True)
+print(False and False)
+print("--------------")
+
+#NOT OR
+print("NOT OR")
+print("--------------")
+print(not (True or False))
+print(not (True or True))
+print(not (False or True))
+print(not (False or False))
+print("--------------")
+
+#NOT AND
+print("NOT AND")
+print("--------------")
+print(not (True and False))
+print(not (True and True))
+print(not (False and True))
+print(not (False and False))
+print("--------------")
