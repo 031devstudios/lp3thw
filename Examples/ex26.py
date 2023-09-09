c69577ac52b093c65b0503cc3156ad3e41b7955e
Josh Robb <031devstudios@gmail.com>
2023-09-09 20:24:02 +0200

    Updates ex26.py

--- Examples/ex26.py
+++ Examples/ex26.py
@@ -74,16 +74,16 @@ cats = 30
 dogs = 15
 
 
-if cats > people:
+if people < cats:
     print("Too many cats! The world is doomed!")
 
-if cats < people:
+if people > cats:
     print("Not many cats! The world is saved!")
 
-if dogs > people:
+if people < dogs:
     print("The world is drooled on!")
 
-if dogs < people:
+if people > dogs:
     print("The world is dry!")
 
 
