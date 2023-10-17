5f49c52c89fe78088ba3e4c98609c6c70423043a
Josh Robb <031devstudios@gmail.com>
2023-10-17 21:00:01 +0200

    Added ex37.py

---
+++ Examples/ex37.py
@@ -0,0 +1,63 @@
+# and - Checks if one statement as well as another is true or False
+x = True
+y = True
+if x and y == True:
+    z = True
+
+# as - used to create an alias
+
+# assert - used for debugging to check if something is true
+
+# break - stops a loop straight away
+
+# class - used to define a new class (object)
+
+# continue - skips the remainder of the loop and starts from the top_( runs again )
+
+# def - used to define a new function
+
+# del - used to del objects from lists etc.
+
+# elif - alternative condition / part of the if statement 
+
+# else - ending of an if statement when none of the conditions are true
+
+# except - if an exception happend do the following / used to print custom errors
+
+# exce - runs a string as python exec 'print("example")'
+
+# finally - normally used after a try...expect block. will run no matter the outcome
+
+# for - used to create a for-loop
+
+# from - used for importing modules
+
+# global - used to declare global variables that can be used inside Functions
+
+# if - used to create an if-statement or Condition
+
+# import - used to import modules
+
+# in - checks if a variable is inside a list
+
+# is - used as an equality test or Check
+
+# lambda - 
+
+# not - inverts the true to false or visa versa
+
+# or - used in logic to check if 1 or another condition is true
+
+# pass - used when there is no code in a function. empty Block
+
+# print - function to print out strings or variables
+
+# raise - raises an error under certain conditions
+
+# try - tries a block of code, if theres an error it runs whatever is in except Block
+
+# while - used for while loops
+
+# with - 
+
+# yield - suspends a function and returns a value. can continue where it left off
